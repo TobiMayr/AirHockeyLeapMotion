5e733f6b1830541bee53c08629285e2d79b40e12
Sandro <Sandro.96@gmx.de>
2018-01-04 20:55:20 +0100

    Add pause menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,7 +10,7 @@ public class GameManager : MonoBehaviour {
     public float PlayerScore;
     GameObject puck, player, ai, pS, eS, powerup_w; //pS - playerScore, eS - enemyScore
     private bool isActive = false;                 // Use this for initialization
-    public GameObject GameEndMenu;
+    public GameObject GameEndMenu, PauseMenu;
     public enum GameMode { Match, Arcade};
     public static GameMode selectedMode;
     private static GameManager instance;
@@ -32,18 +32,42 @@ public class GameManager : MonoBehaviour {
         eS = GameObject.Find("AIScore");
         powerup_w = GameObject.Find("powerup_w");
         GameEndMenu.SetActive(false);
+        PauseMenu.SetActive(false);
+
+
     }
 	
 	// Update is called once per frame
 	void Update () {
         pS.gameObject.GetComponent<TextMesh>().text = "YOU:" + PlayerScore;
         eS.gameObject.GetComponent<TextMesh>().text = "OPP:" + AIScore;
-
+        //endgame menu visible
         if(PlayerScore >= 10)
         {
             Time.timeScale = 0;
             GameEndMenu.SetActive(true);
 
+        }
+
+        //PauseMenu visible
+        if (Time.timeScale == 1)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Time.timeScale = 0;
+                //isActive = true;
+                PauseMenu.SetActive(true);
+            }
+        }
+
+        else if (Time.timeScale == 0)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                //isActive = false;
+                Time.timeScale = 1;
+                PauseMenu.SetActive(false);
+            }
         }
     }
 
@@ -68,12 +92,24 @@ public class GameManager : MonoBehaviour {
     {
         Time.timeScale = 1;
         GameEndMenu.SetActive(false);
+        PauseMenu.SetActive(false);
         puck.gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0f);//stop the striker
         player.transform.position = new Vector3(0.88f, player.transform.position.y, -8.17f);
         ai.transform.position = new Vector3(0.88f, ai.transform.position.y, 6.65f);
         puck.transform.position = new Vector3(0.06f, puck.transform.position.y, -2.59f);
        
         PlayerScore = 0;
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        PauseMenu.SetActive(false);
     }
 
 }
