b819da77f48002666de57b96c16a6dcfd4d740a1
TobiMayr <tobistian@gmail.com>
2018-01-25 15:32:31 +0100

    create UI, start HighScores Script

---
+++ Assets/Scripts/HighScores.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighScores : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void LoadHighScores()
+    {
+        string highscoresSerialized = PlayerPrefs.GetString("highscores", "");
+        Dictionary<string, int> highscores = MyUnserialize(highscoresSerialized);
+
+        // Update highscores.
+        highscores["Xavier"] = 100;
+
+        // Show highscores.
+        foreach (var entry in highscores)
+        {
+            print(entry.Key + ": " + entry.Value);
+        }
+
+    }
+
+    private Dictionary<string, int>  MyUnserialize(string stringy)
+    {
+        return new Dictionary<string, int>();
+    }
+}
